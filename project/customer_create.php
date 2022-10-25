3b8567b8b65f09f221c9c83c81a333447b032c9c
NG ZHI KAI <ngzhikai0308.e.newera.edu.my>
2022-10-26 06:40:54 +0800

    customer

--- project/customer_create.php
+++ project/customer_create.php
@@ -9,6 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <script>
+        $(function() {
+            $(".datepicker").datepicker({
+                dateFormat: 'yy-mm-dd'
+            });
+        });
+    </script>
+
 </head>
 
 <body>
@@ -33,6 +45,113 @@
             </div>
         </nav>
 
+        <div class="container">
+            <div class="page-header">
+                <h1>Create profile</h1>
+            </div>
+
+            <?php
+
+            if ($_POST) {
+                // include database connection
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $first_name = $_POST['first_name'];
+                $last_name = $_POST['last_name'];
+                $gender = $_POST['gender'];
+                $date_of_birth = $_POST['date_of_birth'];
+                $account_status = $_POST['account_status'];
+
+                if ($username == "" || $password == "" || $first_name == "" || $last_name == "" || $gender == "" || $date_of_birth == "" || $account_status == "") {
+                    echo "Please make sure all fields are not empty";
+                } else {
+
+                    include 'config/database.php';
+                    try {
+                        // insert query
+                        $query = "INSERT INTO customers SET username=:username, password=:password, first_name=:first_name, last_name=:last_name, gender=:gender,  date_of_birth=:date_of_birth, account_status=:account_status";
+                        // prepare query for execution
+                        $stmt = $con->prepare($query);
+                        // bind the parameters
+                        $stmt->bindParam(':username', $username);
+                        $stmt->bindParam(':password', $password);
+                        $stmt->bindParam(':first_name', $first_name);
+                        $stmt->bindParam(':last_name', $last_name);
+                        $stmt->bindParam(':gender', $gender);
+                        $stmt->bindParam(':date_of_birth', $date_of_birth);
+                        $stmt->bindParam(':account_status', $account_status);
+                        // Execute the query
+                        if ($stmt->execute()) {
+                            echo "<div class='alert alert-success'>Record was saved.</div>";
+                        } else {
+                            echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                        }
+                    }
+
+                    // show error
+                    catch (PDOException $exception) {
+                        die('ERROR: ' . $exception->getMessage());
+                    }
+                }
+            }
+
+            ?>
+
+            <!-- html form here where the product information will be entered -->
+            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+                <table class='table table-hover table-responsive table-bordered'>
+                    <tr>
+                        <td>Username</td>
+                        <td><input type='text' name='username' class='form-control' /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type='password' name='password' class='form-control' /></td>
+                    </tr>
+                    <tr>
+                        <td>First Name</td>
+                        <td><input type='text' name='first_name' class='form-control' /></td>
+                    </tr>
+                    <tr>
+                        <td>Last Name</td>
+                        <td><input type='text' name='last_name' class='form-control' /></td>
+                    </tr>
+                    <tr>
+                        <td>Gender</td>
+                        <td>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="gender1">
+                                <label class="form-check-label" for="gender1">
+                                    Male
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="gender2">
+                                <label class="form-check-label" for="gender2">
+                                    Female
+                                </label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Date Of Birth</td>
+                        <td><input type='text' name='date_of_birth' class='form-control datepicker' /></td>
+                    </tr>
+                    <tr>
+                        <td>Account Status</td>
+                        <td><textarea type='text' name='account_status' class='form-control'></textarea></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type='submit' value='Save' class='btn btn-primary' />
+                            <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <!-- end .container -->
 
     </div>
 
