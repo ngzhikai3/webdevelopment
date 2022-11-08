abaddb7ccad1f0d8833e4aee01ed62417ffcaf2d
NG ZHI KAI <ngzhikai0308.e.newera.edu.my>
2022-11-09 00:46:32 +0800

    added login function

--- project/login.php
+++ project/login.php
@@ -16,101 +16,108 @@
 
     <div class="container-fluid px-0">
 
+        <h1 class="text-center my-5">Please sign in</h1>
+
         <div class="container mt-5">
             <?php
-            if ($_POST) {
-                // include database connection
-                $user_name = $_POST['username'];
-                $pass_word = $_POST['password'];
 
-                $flag = 0;
-                if ($user_name == "") {
-                    echo "Please enter your username";
-                    $flag = 1;
-                }
-                $space = " ";
-                $word = $_POST['username'];
-                if (strpos($word, $space) !== false) {
-                    echo "Username not space allow";
-                    $flag = 1;
-                } elseif (strlen($user_name) < 6) {
-                    echo "Username need at least 6 charecter";
-                    $flag = 1;
-                }
+            include 'config/database.php';
+            session_start();
+            if (isset($_POST['username']) && isset($_POST['password'])) {
 
-                if ($pass_word == "") {
-                    echo "Please enter your password";
-                    $flag = 1;
-                } elseif (!preg_match('/[A-Z]/', $pass_word)) {
-                    echo "Password need include uppercase";
-                    $flag = 1;
-                } elseif (!preg_match('/[a-z]/', $pass_word)) {
-                    echo "Password need include lowercase";
-                    $flag = 1;
-                } elseif (!preg_match('/[0-9]/', $pass_word)) {
-                    echo "Password need include number";
-                    $flag = 1;
-                } elseif (strlen($pass_word) < 8) {
-                    echo "Password need at least 8 charecter";
-                    $flag = 1;
-                }
+                $username = ($_POST['username']);
+                $password = ($_POST['password']);
 
-                if ($flag == 0) {
-                }
-            }
+                $select = " SELECT username, password, account_status FROM customers WHERE username = '$username' && password = '$password'";
+                $result = mysqli_query($mysqli, $select);
+                $row = mysqli_fetch_assoc($result);
 
+                if (mysqli_num_rows($result) == 1) {
+                    if ($row['username'] === $username && $row['password'] === $password && $row['account_status'] == "active") {
+                        header("Location: index.php");
+                    } elseif ($row['username'] === $username && $row['password'] === $password && $row['account_status'] != "active") {
+                        echo "<h3 class='alert alert-danger'>Your account is inactive.</h3>";
+                    }
+                }
+                $check_username = " SELECT username FROM customers WHERE username = '$username'";
+                $result2 = mysqli_query($mysqli, $check_username);
+                $row = mysqli_fetch_assoc($result2);
+                if (mysqli_num_rows($result2) == 0) {
+                    echo "<h3 class='alert alert-danger'>Your username is incorrect.</h3>";
+                } else {
+                    $check_password = " SELECT password FROM customers WHERE password = '$password'";
+                    $result3 = mysqli_query($mysqli, $check_password);
+                    $row = mysqli_fetch_assoc($result3);
+                    if (mysqli_num_rows($result3) == 0) {
+                        echo "<h3 class='alert alert-danger'>Your password is incorrect.</h3>";
+                    }
+                }
+            };
             ?>
-            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
-                <h1 class="text-center">Please sign in</h1>
-                <div class="my-3">
-                    <h3>Username</h3>
-                    <select class="form-select form-select" aria-label=".form-select example" name="username">
-                        <?php
 
-                        include 'config/database.php';
-                        // select all data
-                        $query = "SELECT username FROM customers ORDER BY username DESC";
-                        $stmt = $con->prepare($query);
-                        $stmt->execute();
-                        // this is how to get number of rows returned
-                        $num = $stmt->rowCount();
-                        //check if more than 0 record found
-                        if ($num > 0) {
-                            // data from database will be here
+            <?php
+            // include database connection
+            /*include 'config/database.php';
+            session_start();
+            if (isset($_POST['username']) && isset($_POST['password'])) {
+
+                $username = ($_POST['username']);
+                $password = ($_POST['password']);
+
+                $select = " SELECT username, password, account_status FROM customers WHERE username = '$username' && password = '$password' ";
+                $result = mysqli_query($mysqli, $select);
+                $row = mysqli_fetch_assoc($result);
+
+                if (mysqli_num_rows($result) == 1) {
+                    if ($row['username'] === $username && $row['password'] === $password) {
+                        if ($row['account_status'] != "active") {
+                            echo "<div class='alert alert-danger'>Your account is closed.</div>wrong password";
                         } else {
-                            echo "<div class='alert alert-danger'>No records found.</div>";
+                            header("location:index.php");
                         }
-                        //new
-                        echo "<option selected>Username</option>"; //start dropdown
-                        // table body will be here
-                        // retrieve our table contents
-                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                            // extract row
-                            // this will make $row['firstname'] to just $firstname only
-                            extract($row);
-                            // creating new table row per record
-                            echo "<option value=\"$username\">$username</option>";
-                        }
-                        // end table
-                        echo "</option>";
-                        ?>
-                    </select>
-                </div>
+                    }
+                }
+                $findname = " SELECT username FROM customers WHERE username = '$username'";
+                $result2 = mysqli_query($mysqli, $findname);
+                $row = mysqli_fetch_assoc($result2);
+                if (mysqli_num_rows($result2) == 0) {
+                    echo "<div class='alert alert-danger'>Wrong Username.</div>wrong password";
+                } else {
+                    $findpass = " SELECT password FROM customers WHERE password = '$password'";
+                    $result3 = mysqli_query($mysqli, $findpass);
+                    $row = mysqli_fetch_assoc($result3);
+                    if (mysqli_num_rows($result3) == 0) {
 
-                <div class="my-3">
-                    <form>
-                        <div>
-                            <h3>Password</h3>
-                            <td><input type='password' name='password' class='form-control' /></td>
-                        </div>
-                    </form>
-                </div>
+                        echo "<div class='alert alert-danger'>Wrong Password.</div>wrong password";
+                    }
+                }
+            }*/
+            ?>
 
-                <div class="text-center my-3">
-                    <a href="index.php" class='btn btn-primary w-50 m-1'>Submit</a>
-                </div>
+            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+                <table class='table table-hover table-responsive table-bordered '>
+                    <div class="form-floating  ">
+                        <input type="text" class="form-control" id="floatingInput" name="username">
+                        <label for="floatingInput">Username</label>
+                    </div>
+
+                    <div class="form-floating ">
+                        <input type="password" class="form-control" id="floatingPassword" name="password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+
+                    <div class="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me"> Remember me
+                        </label>
+                    </div>
+
+                    <div class="text-center my-3">
+                        <button class="w-50 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    </div>
             </form>
         </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
